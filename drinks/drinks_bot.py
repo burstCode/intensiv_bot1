ee8cf70d8fd891437dc1a1a4aeb74984331b9c32
burstCode <yourewelcomex@gmail.com>
2023-10-03 17:49:18 +0300

    Bot_Drinks_Eng

--- drinks/drinks_bot.py
+++ drinks/drinks_bot.py
@@ -14,48 +14,48 @@ dp = Dispatcher(bot)
 class KeyboardMain(ReplyKeyboardMarkup):
     def __init__(self):
         super().__init__(resize_keyboard=True, one_time_keyboard=True)
-        self.add(KeyboardButton('Напитки'))
-        self.add(KeyboardButton('Закуски'))
+        self.add(KeyboardButton('Drink'))
+        self.add(KeyboardButton('Food'))
 
 
 class KeyboardDrink(ReplyKeyboardMarkup):
     def __init__(self):
         super().__init__(resize_keyboard=True, one_time_keyboard=True)
-        self.insert(KeyboardButton('Сок'))
-        self.insert(KeyboardButton('Газировка'))
-        self.add(KeyboardButton('Назад'))
+        self.insert(KeyboardButton('Juice'))
+        self.insert(KeyboardButton('Sparkly water'))
+        self.add(KeyboardButton('Back'))
 
 
 class KeyboardSnack(ReplyKeyboardMarkup):
     def __init__(self):
         super().__init__(resize_keyboard=True, one_time_keyboard=True)
-        self.insert(KeyboardButton('Чипсы'))
-        self.insert(KeyboardButton('Бургер'))
-        self.add(KeyboardButton('Назад'))
+        self.insert(KeyboardButton('Chips'))
+        self.insert(KeyboardButton('Burger'))
+        self.add(KeyboardButton('Back'))
 
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     user_name = message.from_user.username
     await message.answer(
-        f'Приветствую, {user_name}! \nВыбери нужное',
+        f'Greetings, {user_name}! \nChoose your needs',
         reply_markup=KeyboardMain()
     )
 
 
-@dp.message_handler(text='Напитки')
+@dp.message_handler(text='Drink')
 async def get_drink(message: types.Message):
-    await message.answer('Выбери напиток', reply_markup=KeyboardDrink())
+    await message.answer('Choose drink', reply_markup=KeyboardDrink())
 
 
-@dp.message_handler(text='Закуски')
+@dp.message_handler(text='Food')
 async def get_drink(message: types.Message):
-    await message.answer('Выбери закуску', reply_markup=KeyboardSnack())
+    await message.answer('Choose snack', reply_markup=KeyboardSnack())
 
 
-@dp.message_handler(text='Назад')
+@dp.message_handler(text='Back')
 async def get_drink(message: types.Message):
-    await message.answer('Вы вышли в главное меню', reply_markup=KeyboardMain())
+    await message.answer('You quited to the main menu', reply_markup=KeyboardMain())
 
 
 if __name__ == '__main__':
